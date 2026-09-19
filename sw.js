fdaaad1cb3e40ba47b8d030a6fed23aac21c7b52
fabiveL00 <fabian.schweikert04@gmail.com>
2026-09-19 21:06:48 +0200

    Add files via upload
    Flip7 Spieleranzahl update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Spieleabend – Offline-Cache
-const CACHE = 'spieleabend-v6';
+const CACHE = 'spieleabend-v7';
 const CORE = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
