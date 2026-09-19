487fe31d741086381a4cf4e23dac86568d4c25fa
fabiveL00 <fabian.schweikert04@gmail.com>
2026-09-19 18:12:08 +0200

    Theme changes and wizard update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Spieleabend – Offline-Cache
-const CACHE = 'spieleabend-v3';
+const CACHE = 'spieleabend-v5';
 const CORE = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
